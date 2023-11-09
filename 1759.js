180a8f44cd33887bf01e7f30a012a7edf1234843
chris Taylor <ctay456a@gmail.com>
2023-11-09 08:53:11 -0500

    Create 1759.js

---
+++ 1759.js
@@ -0,0 +1,23 @@
+var countHomogenous = function(s) {
+    let res = 0;
+    let start = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] !== s[start]) {
+            let appear = i - start;
+            while (appear > 0) {
+                res += appear;
+                appear -= 1;
+            }
+            start = i;
+        }
+    }
+
+    let appear = s.length - start;
+    while (appear > 0) {
+        res += appear;
+        appear -= 1;
+    }
+
+    return res % (Math.pow(10, 9) + 7);
+};
